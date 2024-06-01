7b21c1add76abffdf338e6503a7ab00eb78d43d5
xiaolinyin <xiaolinyin@bdgy.net>
2024-06-01 18:22:22 +0800

    13.2.3 让外星人出现在屏幕上

--- game_functions.py
+++ game_functions.py
@@ -4,7 +4,7 @@ from bullet import Bullet
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """ 响应按键 """
-    print(event.key)
+    # print(event.key)
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
@@ -12,7 +12,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
     elif event.key == pygame.K_q:
-        print(event.key)
+        # print(event.key)
         sys.exit()
 
 def check_keyup_events(event, ship):
@@ -34,7 +34,7 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """ 更新屏幕上的图像，并切换到新屏幕 """
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -42,6 +42,7 @@ def update_screen(ai_settings, screen, ship, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
